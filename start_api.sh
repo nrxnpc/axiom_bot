3702ede061c9694121eea505a93512d2eae442e1
nrxnpc <132189553+nrxnpc@users.noreply.github.com>
2025-06-26 00:48:33 -0700

    Add files via upload

---
+++ start_api.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/bot4admin/nsp_qr_bot
+source venv/bin/activate
+python api_server.py
---
+++ start_bot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/bot4admin/nsp_qr_bot
+source venv/bin/activate
+python qr_bot.py
